3364e33f5517418cef29b1bdf3f9fb6f1f54ff1d
Yura95st <yura95@gmail.com>
2013-03-24 00:09:12 +0200

    test3

--- CockroachRacing/brocker.h
+++ CockroachRacing/brocker.h
@@ -17,13 +17,5 @@ private:
 	int brokerRate;
 	double brokerProfit;
 	int stakeArray[];
-
-	/*List<Competitor>* competitorList;
-	List<Competitor>* competitorActualList;
-	List<Stake>* stakeList;
-	List<BrockerResult>* resultsList;
-	int brokerRate;
-	double brokerProfit;
-	int stakeArray[];*/
 };
 
