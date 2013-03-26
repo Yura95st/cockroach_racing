cd1c47198731b9918fe8df515fe83ac47905b0cb
Yura95st <yura95@gmail.com>
2013-03-26 16:58:33 +0200

    implemented methods: constructor, destructor, getCompetitorsList(), getCompetitorsList(), getCompetitorsList(), getCompetitorsList(), getCompetitorsList()$ added new method copyList() - for list copying

--- CockroachRacing/brocker.h
+++ CockroachRacing/brocker.h
@@ -8,22 +8,23 @@ class Brocker
 public:
 	Brocker();
 	~Brocker();
+	List<Competitor>* getCompetitorsList();
+	List<Competitor>* getCompetitorsActualList();
+	List<BrockerResult>* getResultsList();
+	int* getAvailableStakes();
+	int putTheBet();
+	int getMoney();
 
 private:
-	List<Competitor>* competitorList;
-	List<Competitor>* competitorActualList;
+	List<Competitor>* competitorsList;
+	List<Competitor>* competitorsActualList;
 	List<Stake>* stakeList;
 	List<BrockerResult>* resultsList;
 	int brokerRate;
 	double brokerProfit;
-	int stakeArray[];
+	int* stakeArray;
 
-	/*List<Competitor>* competitorList;
-	List<Competitor>* competitorActualList;
-	List<Stake>* stakeList;
-	List<BrockerResult>* resultsList;
-	int brokerRate;
-	double brokerProfit;
-	int stakeArray[];*/
+	template <class T>
+	int copyList(List<T>* listFrom, List<T>* &listTo);
 };
 
